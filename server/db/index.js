962e2706f8ad22ebc052187c210f15b572f00985
Fabian Rizzi <82421798+Fabian-Rizzi@users.noreply.github.com>
2022-09-15 10:06:14 -0300

    postgresql connection

---
+++ server/db/index.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'alkemychallenge',
+    password: 'SQLdatabase',
+    port: 5432,
+}
+    
+)
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
+
--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
 const express = require("express");
-
+const { nextTick } = require("process");
 
 const app = express();
+
+//Middleware
+
+app.use(express.json());
+
+
+app.use((req, res, next) => {
+    console.log('middleware');
+    next();
+});
+
+
 
 //Get transactions
 
@@ -15,9 +27,28 @@ app.get("/api/v1/transactions", (req, res) => {
     });
 });
 
+//Get a transaction
+app.get("/api/v1/transactions/:id", (req, res) => {
+    console.log(req.params.id);
+
+    try {
+
+    } catch (err) {
+
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            
+        },
+    });
+});
+
+
 //Create transaction
 
 app.post("/api/v1/transactions/create", (req, res) => {
+    console.log(req.body);   
     res.status(201).json({
         status: "success",
         data: {
@@ -27,9 +58,11 @@ app.post("/api/v1/transactions/create", (req, res) => {
 });
 
 
-//edit transaction
+//Edit transaction
 
 app.put("/api/v1/transactions/:id/edit", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
     res.status(200).json({
         status: "success",
         data: {
@@ -39,13 +72,15 @@ app.put("/api/v1/transactions/:id/edit", (req, res) => {
 });
 
 
-//delete transaction
+//Delete transaction
 
 app.delete("/api/v1/transactions/:id", (req, res) => {
     res.status(204).json({
         status: "success"
     });
 });
+
+
 
 
 
