6e0dfd132b700bdd7fa07f7347ab091da73f1a48
Fabian Rizzi <faberizzi92@gmail.com>
2022-10-06 21:07:52 -0300

    heroku front end

--- client/src/apis/TransactionFinder.js
+++ client/src/apis/TransactionFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api/v1/transactions",
+    baseURL: "https://wifinances-alk.herokuapp.com/api/v1/transactions",
 });
