6d09d15e4073c387375ba8bff9b7c14507ab2260
Fabian Rizzi <faberizzi92@gmail.com>
2022-10-13 16:30:05 -0300

    heroku baseurl added

--- client/src/apis/TransactionFinder.js
+++ client/src/apis/TransactionFinder.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api/v1/transactions",
+    // local url:
+    // baseURL: "http://localhost:3000/api/v1/transactions",
+    
     // url for deployment:
-    // baseURL: "https://wifinances-alk.herokuapp.com/api/v1/transactions",
+    baseURL: "https://wifinances-alk.herokuapp.com/api/v1/transactions",
 });
